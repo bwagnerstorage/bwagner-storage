92ddfcde32414581fe9080766c16f170c64f2d81
bwagnerstorage <132395064+bwagnerstorage@users.noreply.github.com>
2023-05-17 09:12:13 -0500

    Update pw.js

--- menu/ap/APTester/pw.js
+++ menu/ap/APTester/pw.js
@@ -3,6 +3,8 @@ var pw = "view-source:";
 function testResults(form) {
     var iv1 = form.inputbox1.value;
   
+    document.getElementById("pain").innerHTML = form + " " + iv1;
+    
     if(iv1 == pw) {
       window.location.href = "menu/ap/APTester/mainAP.html";
     }
